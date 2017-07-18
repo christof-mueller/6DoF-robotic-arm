aa1a21ec938c8e9648e8be8e0194043c808870e5
christof-mueller <christof.mueller@gmail.com>
2017-07-18 19:04:33 +0200

    type: feat
    Added manual control for base joint.

--- RoboticArm/Form1.Designer.cs
+++ RoboticArm/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.baseSlider = new System.Windows.Forms.TrackBar();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.baseLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.baseSlider)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // baseSlider
+            // 
+            this.baseSlider.Location = new System.Drawing.Point(104, 19);
+            this.baseSlider.Maximum = 90;
+            this.baseSlider.Minimum = -90;
+            this.baseSlider.Name = "baseSlider";
+            this.baseSlider.Size = new System.Drawing.Size(506, 45);
+            this.baseSlider.TabIndex = 1;
+            this.baseSlider.ValueChanged += new System.EventHandler(this.baseSlider_ValueChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(641, 379);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.baseLabel);
+            this.tabPage2.Controls.Add(this.baseSlider);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(633, 353);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Manual Mode";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // logBox
+            // 
+            this.logBox.Location = new System.Drawing.Point(12, 397);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(614, 96);
+            this.logBox.TabIndex = 3;
+            this.logBox.Text = "";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(624, 397);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.clearButton.Size = new System.Drawing.Size(25, 95);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "C L E A R";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // baseLabel
+            // 
+            this.baseLabel.AutoSize = true;
+            this.baseLabel.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.baseLabel.Location = new System.Drawing.Point(15, 19);
+            this.baseLabel.Name = "baseLabel";
+            this.baseLabel.Size = new System.Drawing.Size(68, 25);
+            this.baseLabel.TabIndex = 2;
+            this.baseLabel.Text = "Base";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 504);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.baseSlider)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.TrackBar baseSlider;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label baseLabel;
     }
 }
 
--- RoboticArm/Form1.cs
+++ RoboticArm/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tinkerforge;
 
 namespace RoboticArm
 {
     public partial class Form1 : Form
     {
+        private static string HOST = "localhost";
+        private static int PORT = 4223;
+        private static string UID = "6wT1xU";
+
+        private IPConnection ipcon;
+        private BrickServo servo;
+
         public Form1()
         {
             InitializeComponent();
+            InitHardware();
         }
+
+        private void InitHardware()
+        {
+            // Connection to brickd
+            ipcon = new IPConnection(); 
+            servo = new BrickServo(UID, ipcon); 
+            ipcon.Connect(HOST, PORT); 
+
+            servo.SetOutputVoltage(5000); // 5V
+
+            // servo0 = HIP-Joint
+            servo.SetDegree(0, -9000, 9000);
+            servo.SetPulseWidth(0, 1000, 2000);
+            servo.SetPeriod(0, 19500);
+            servo.SetAcceleration(0, 65535); // Full Accel
+            servo.SetVelocity(0, 65535); // Full speed
+        }
+
+   
+
+        private void baseSlider_ValueChanged(object sender, EventArgs e)
+        {
+            servo.SetPosition(0, (short)(baseSlider.Value * 100)); // Set to most right position
+            servo.Enable(0);
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            logBox.Clear();
+        }
+
+        private void Log(string message)
+        {
+            logBox.AppendText(message + Environment.NewLine);
+        }
+            
     }
 }
